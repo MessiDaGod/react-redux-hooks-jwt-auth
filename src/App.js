67abbd1165277ab7dfcbfe7c25584615c908a601
joeshakely <joeshakely@gmail.com>
2022-06-04 08:22:33 -0700

    changes

--- src/App.js
+++ src/App.js
@@ -61,7 +61,7 @@ const App = () => {
       <div>
         <nav className="navbar navbar-expand navbar-dark bg-dark">
           <Link to={"/"} className="navbar-brand">
-            bezKoder
+            Shakely Consulting
           </Link>
           <div className="navbar-nav mr-auto">
             <li className="nav-item">
